6bb7e208b5c1564f382deef87c9b640d3fd9da35
vedantmagarde <vedantmagarde@gmail.com>
2026-07-28 23:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 37.2 MB (82.45%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -8,6 +8,7 @@ public:
         fun(arr, root->right);
     }
     
+
     bool findTarget(TreeNode* root, int k) {
         if (root != NULL && root->left == NULL && root->right == NULL)
             return false;
@@ -23,23 +24,5 @@ public:
                 r--;
         }
         return false;
-
-        // vector<int> brr;
-        // fun(arr, root);
-        // for (int i = 0; i < arr.size(); i++) {
-        //     brr.push_back(k - arr[i]);
-        // }
-        // reverse(brr.begin(), brr.end());
-        // int i1 = 0, i2 = 0, n = arr.size();
-        // while (i1 < n && i2 < n) {
-        //     if (arr[i1] == brr[i2] && arr[i1] * 2 != k)
-        //         return true;
-        //     if (arr[i1] < brr[i2]) {
-        //         i1++;
-        //     } else {
-        //         i2++;
-        //     }
-        // }
-        // return false;
     }
 };
