fd7f17f5b69a767fc3ae62907ce85db9140a3882
vedantmagarde <vedantmagarde@gmail.com>
2026-07-28 23:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 37.6 MB (38.54%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    void fun(vector<int>& arr, TreeNode* root) {
-        if (root == NULL)
-            return;
-        fun(arr, root->left);
-        arr.push_back(root->val);
-        fun(arr, root->right);
+    void leftwala(TreeNode* root, stack<TreeNode*>& leftStack) {
+        while (root != NULL) {
+            leftStack.push(root);
+            root = root->left;
+        }
     }
-
+    void rightwala(TreeNode* root, stack<TreeNode*>& rightStack) {
+        while (root != NULL) {
+            rightStack.push(root);
+            root = root->right;
+        }
+    }
     bool findTarget(TreeNode* root, int k) {
+        if (!root)
+            return false;
 
+        stack<TreeNode*> leftStack, rightStack;
 
-        vector<int> arr;
-        fun(arr, root);
-        int l = 0, r = arr.size() - 1;
-        while (l < r) {
-            if (arr[l] + arr[r] == k)
+        leftwala(root, leftStack);
+        rightwala(root, rightStack);
+
+        while (leftStack.top()->val < rightStack.top()->val) {
+            int sum = leftStack.top()->val + rightStack.top()->val;
+
+            if (sum == k)
                 return true;
-            else if (arr[l] + arr[r] < k)
-                l++;
-            else
-                r--;
+                
+            else if (sum < k) {
+                TreeNode* temp = leftStack.top();
+                leftStack.pop();
+                leftwala(temp->right, leftStack);
+            } else {
+                TreeNode* temp = rightStack.top();
+                rightStack.pop();
+                rightwala(temp->left, rightStack);
+            }
         }
         return false;
     }
