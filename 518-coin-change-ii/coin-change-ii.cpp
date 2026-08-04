ab451ae9fb7a562bce82bd8a5359244de957f081
vedantmagarde <vedantmagarde@gmail.com>
2026-08-04 09:10:27 +0530

    Time: 156 ms (5.15%) | Memory: 277.8 MB (5.77%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int fun(int amount, vector<int>& coins, int idx, vector<vector<int>>& dp) {
-        if (amount == 0)
+    int fun(int amt, vector<int>& coins, int idx, vector<vector<int>>& dp) {
+        if (amt == 0)
             return 1;
 
-        if (amount < 0 || idx >= coins.size())
+        if (amt < 0 || idx >= coins.size())
             return 0;
 
-        if (dp[amount][idx] != -5)
-            return dp[amount][idx];
+        if (dp[amt][idx] != -4)
+            return dp[amt][idx];
 
-        int skip = fun(amount, coins, idx + 1, dp);
+        int skip = fun(amt, coins, idx + 1,dp);
 
         int take = 0;
-        if (amount >= coins[idx]) {
-            take = fun(amount - coins[idx], coins, idx, dp);
-        }
-        return dp[amount][idx] = skip + take;
+        if (amt - coins[idx] >= 0)
+            take = fun(amt - coins[idx], coins, idx,dp);
+        return dp[amt][idx] = skip + take;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(5005, vector<int>(305, -5));
-        return fun(amount, coins, 0, dp);
+        vector<vector<int>> dp(5005, vector<int>(305, -4));
+        return fun(amount, coins, 0,dp);
     }
 };
