4851dc72e602eabb95063bf9e9c8d030123f627a
vedantmagarde <vedantmagarde@gmail.com>
2026-08-16 23:56:51 +0530

    Time: 85 ms (36.91%) | Memory: 88.9 MB (49.55%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,20 +3,26 @@ public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.size() == 0)
             return 0;
-        sort(nums.begin(), nums.end());
+        unordered_set<int> st;
+
+        for (auto x : nums) {
+            st.insert(x);
+        }
         int ans = 0;
         int len = 1;
-        for (int i = 0; i + 1 < nums.size(); i++) {
+        for (auto x : st) {
 
-            if (nums[i + 1] == nums[i])
-                continue;
-            if (nums[i + 1] - nums[i] == 1) {
-                len++;
+            if (st.find(x - 1) == st.end()) {
+
+                int temp = x;
+                while (st.find(temp+ 1) != st.end()) {
+                    len++;
+                    temp++;
+                }
+
                 ans = max(len, ans);
-            } else {
                 len = 1;
             }
-            // cout << len << " " << ans << endl;
         }
         ans = max(len, ans);
         return ans;
